d8fa6fdc3d3e977a9d5ff4752868084e6a1b8976
David <David@Meow>
2016-10-07 20:38:24 -0400

    new commit

---
+++ src/quicktables/Course.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quicktables;
+
+/**
+ *
+ * @author David
+ */
+public class Course {
+    
+    private String course;
+    private String room;
+    private String teacher;
+    
+    Course(String course, String room, String teacher){
+        this.course = course;
+        this.room = room;
+        this.teacher = teacher;
+    }
+    
+    String getCourse(){
+        return course;
+    }
+    
+    String getRoom(){
+        return course;
+    }
+    
+    String getTeacher(){
+        return course;
+    }
+}
---
+++ src/quicktables/Day.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quicktables;
+import java.io.*;
+/**
+ *
+ * @author David
+ */
+public class Day implements Serializable {
+    
+    private Course[] period = new Course[6];
+}
